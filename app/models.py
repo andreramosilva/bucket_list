141bdc89aade7456e44cfbffae152d2cc2605ddd
Andre Ramos Silva <andre-ramos1994@hotmail.com>
2020-09-02 11:04:26 -0300

    api ready

---
+++ app/models.py
@@ -0,0 +1,35 @@
+# app/models.py
+
+from app import db
+
+
+class Bucketlist(db.Model):
+    """This class represents the bucketlist table."""
+
+    __tablename__ = 'bucketlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        """initialize with name."""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Bucketlist.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Bucketlist: {}>".format(self.name)
